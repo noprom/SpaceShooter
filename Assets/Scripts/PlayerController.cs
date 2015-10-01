f938efb5bb9e1ed6cfdf56a7df63e124b0942387
noprom <tyee.noprom@qq.com>
2015-10-01 16:22:37 +0800

    make Boundry serializable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundry {
+	public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Boundry boundry;
+
 //	// Use this for initialization
 //	void Start () {
 //	
@@ -18,8 +25,14 @@ public class PlayerController : MonoBehaviour {
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+		// Setup movement
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
 
+		// Setup position
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			Mathf.Clamp(GetComponent<Rigidbody> ().position.x, boundry.xMin, boundry.xMax),
+			0.0f,
+			Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundry.zMin, boundry.zMax));
 	}
 }
