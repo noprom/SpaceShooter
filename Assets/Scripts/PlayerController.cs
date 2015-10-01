3c1f5622c4572a88df22c6a146dbc7eee730a7a3
noprom <tyee.noprom@qq.com>
2015-10-01 15:55:48 +0800

    add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement;
+
+	}
+}
